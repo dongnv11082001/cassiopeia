200e13cdc362272ae92d1cd67a77c26db7ed70be
dongnv11082001 <hiiamgosu4@gmail.com>
2022-02-21 11:12:05 +0700

    update about page

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import {Route, Routes} from "react-router-dom";
+import { Route, Routes } from "react-router-dom";
 import styled from "styled-components";
 import "./App.css";
 
@@ -13,37 +13,39 @@ import ProductPage from "./pages/ProductPage/ProductPage";
 import GiftPage from "./pages/GiftPage/GiftPage";
 import DiscountsPage from "./pages/DiscountsPage/DiscountsPage";
 import CheckoutPage from "./pages/CheckoutPage/CheckoutPage";
+import About from "./pages/About/About";
 
 function App() {
-    return (
-        <>
-            <AppContainer>
-                <div className={'content'}>
-                    <Header/>
-                    <Navbar/>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                        <Route path="/flowers" element={<FlowerPage/>}/>
-                        <Route path='/plants' element={<PlantPage/>}/>
-                        <Route path='/flowers/:id' element={<ProductPage/>}/>
-                        <Route path={'/gifts'} element={<GiftPage/>} />
-                        <Route path={'/discounts'} element={<DiscountsPage/>} />
-                        <Route path={'/checkout'} element={<CheckoutPage/> } />
-                    </Routes>
-                </div>
-            </AppContainer>
-            <Footer/>
-        </>
-    );
+  return (
+    <>
+      <AppContainer>
+        <div className={"content"}>
+          <Header />
+          <Navbar />
+          <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/flowers" element={<FlowerPage />} />
+            <Route path="/plants" element={<PlantPage />} />
+            <Route path="/flowers/:id" element={<ProductPage />} />
+            <Route path={"/gifts"} element={<GiftPage />} />
+            <Route path={"/discounts"} element={<DiscountsPage />} />
+            <Route path={"/about-us"} element={<About />} />
+            <Route path={"/checkout"} element={<CheckoutPage />} />
+          </Routes>
+        </div>
+      </AppContainer>
+      <Footer />
+    </>
+  );
 }
 
 export default App;
 
 const AppContainer = styled.div`
-    display: flex;
-    justify-content: center;
-    align-item: center;
-  
+  display: flex;
+  justify-content: center;
+  align-item: center;
+
   & .content {
     max-width: 1160px;
     width: 100%;
